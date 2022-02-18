99416b7ce766b850ad33d3fd9463cb91e19e2c34
iljaasd <iljaasd@gmail.com>
2022-02-18 12:20:12 +0100

    Initial commit

---
+++ src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/Assignment2DataAccessAndDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.modandmohamed.assignment_2_dataaccessanddisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2DataAccessAndDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2DataAccessAndDisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modandmohamed/assignment_2_dataaccessanddisplay/Assignment2DataAccessAndDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modandmohamed.assignment_2_dataaccessanddisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2DataAccessAndDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
