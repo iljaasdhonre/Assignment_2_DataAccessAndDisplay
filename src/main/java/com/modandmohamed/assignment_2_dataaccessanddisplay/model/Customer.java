469160215913d068619b15769f145aa4fab058c6
Maghawry <mag.hauwry@gmail.com>
2022-02-18 19:21:08 +0100

    Connection with the Chinook db has been established

--- src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/model/Customer.java
+++ src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/model/Customer.java
@@ -1,10 +1,63 @@
 package com.modandmohamed.assignment_2_dataaccessanddisplay.model;
 
+import java.sql.*;
+
+import static com.modandmohamed.assignment_2_dataaccessanddisplay.database.ConnectionHelper.URL;
+
 public class Customer {
+
+        private Connection connect() {
+            Connection conn = null;
+
+            try {
+                conn = DriverManager.getConnection(URL);
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return conn;
+        }
+
+
+        public void ReadAllCustomers(){
+            String sql = "SELECT * FROM Customer";
+
+            try {
+                Connection conn = this.connect();
+                Statement stmt  = conn.createStatement();
+                ResultSet rs    = stmt.executeQuery(sql);
+
+                // loop through the result set
+                while (rs.next()) {
+                    System.out.println(rs.getInt("CustomerId") +  "\t" +
+                            rs.getString("FirstName") + "\t" +
+                            rs.getString("LastName")  + "\t" +
+                            rs.getString("Country")  + "\t" +
+                            rs.getString("PostalCode")  + "\t" +
+                            rs.getString("Phone")  + "\t" +
+                            rs.getString("Email")
+
+                    );
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+        /**
+         * @param args the command line arguments
+         */
+        public static void main(String[] args) {
+            Customer app = new Customer();
+            app.ReadAllCustomers();
+        }
 
     //private properties
 
     //constructor
+
+    public Customer() {
+    }
 
     //getter and setters
 }
