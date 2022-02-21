c27fd085220b614daf10ab911a9627e7be589f67
Maghawry <mag.hauwry@gmail.com>
2022-02-21 15:49:43 +0100

    Genre model class added.

--- src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/dataaccess/model/CustomerGenre.java
+++ src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/dataaccess/model/CustomerGenre.java
@@ -1,4 +1,27 @@
 package com.modandmohamed.assignment_2_dataaccessanddisplay.dataaccess.model;
 
 public class CustomerGenre {
+    private Integer GenreId;
+    private String Name;
+
+    public CustomerGenre(Integer genreId, String name) {
+        GenreId = genreId;
+        Name = name;
+    }
+
+    public Integer getGenreId() {
+        return GenreId;
+    }
+
+    public void setGenreId(Integer genreId) {
+        GenreId = genreId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
