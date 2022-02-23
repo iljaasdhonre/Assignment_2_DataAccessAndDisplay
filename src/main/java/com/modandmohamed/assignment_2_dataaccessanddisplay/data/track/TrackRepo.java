8832b2496c5be77f6352e2bd3027e09123a8d12d
Maghawry <mag.hauwry@gmail.com>
2022-02-23 21:34:58 +0100

    getTracksByName() method added to TrackRepo

--- src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/data/track/TrackRepo.java
+++ src/main/java/com/modandmohamed/assignment_2_dataaccessanddisplay/data/track/TrackRepo.java
@@ -106,6 +106,46 @@ public class TrackRepo implements ITrackRepo{
 
     @Override
     public Track getTrackByName(String name) {
-        return null;
+
+        Track track = null;
+
+        String sqlQuery = "SELECT * FROM Track WHERE Name LIKE = ?";
+
+        try {
+            //Connect to DB
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection established");
+
+            //Make query
+            PreparedStatement statement = conn.prepareStatement(
+                    sqlQuery);
+
+            //Execute query
+            ResultSet rs = statement.executeQuery();
+
+            //Process result
+            while (rs.next()) {
+                track = new Track(
+                        rs.getInt("TrackId"),
+                        rs.getString("TrackName"),
+                        rs.getString("Composer"),
+                        rs.getInt("Milliseconds"),
+                        rs.getInt("Bytes"),
+                        rs.getDouble("UnitPrice")
+                );
+            }
+        } catch (SQLException sqe) {
+            System.out.println(sqe.getMessage());
+        } finally {
+            try {
+                conn.close();
+                System.out.println("Connection closed");
+            } catch (SQLException sqe) {
+                sqe.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+        return track;
     }
 }
